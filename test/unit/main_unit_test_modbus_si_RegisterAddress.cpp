3ea4cac568781fd2af741cc25e1a238e572763ff
Maik Urbannek <maik.urbannek@cattatech.de>
2025-07-08 09:19:32 +0200

    test::RegisterAddress: Add tofromRaw test.

--- test/unit/main_unit_test_modbus_si_RegisterAddress.cpp
+++ test/unit/main_unit_test_modbus_si_RegisterAddress.cpp
@@ -16,14 +16,26 @@
 
 static constexpr bool debug = false;
 
+template <catta::test::Output OUTPUT>
+static bool checkToFromRaw(catta::test::Test<OUTPUT>& test)
+{
+    const auto input = test.random().template create<catta::modbus::si::RegisterAddress>();
+    const auto output = catta::modbus::si::RegisterAddress::fromRaw(input.raw());
+    if (debug) test.status(catta::tostring::toString(input));
+    if (input != output) return test.failedExpected(output, input, "output");
+    return true;
+}
+
 int main()
 {
     using Type = catta::modbus::si::RegisterAddress;
     using Output = catta::test::ConsoleOutput;
+    const auto test_create = checkToFromRaw<Output>;
     const auto test_tofromstring = catta::test::checkToFromString<Output, Type, debug>;
     const auto test_copymove = catta::test::checkCopyMove<Output, Type, catta::test::NO_CHECK>;
     Output output(catta::tostring::GetName<Type>::name.data());
     int error = 0;
+    if (!catta::test::execute<Output>(test_create, "tofromraw", output, 10000)) error++;
     if (!catta::test::execute<Output>(test_tofromstring, "tofromstring", output, 10000)) error++;
     if (!catta::test::execute<Output>(test_copymove, "copymove", output, 10000)) error++;
     return error;
