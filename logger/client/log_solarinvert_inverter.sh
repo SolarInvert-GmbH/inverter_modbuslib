5024d8bc562a6c601fb1fba1ca66e8f94b6ef0a3
Maik Urbannek <maik.urbannek@cattatech.de>
2025-12-04 10:22:34 +0100

    client::log_solarinvert_inverter: Ignore 0xf…f Messages.

--- logger/client/log_solarinvert_inverter.sh
+++ logger/client/log_solarinvert_inverter.sh
@@ -37,14 +37,19 @@ LOOP_TIME_NANO_SECONDS=$((NANO_FACTOR * LOOP_TIME_SECONDS))
 
 readRegisterToLines()
 {
+    REGISTER_TYPE="${6}"
     RESULT=$("${COMMAND}" --uart "${UART}" --modbusid "${4}" --request "{\"type\":\"readRegister\",\"value\":{\"registerAddress\":\"${1}\"}}" --timeout 50)
-
 
     if [ $? -eq 0 ]; then
         VALUE_INTEGER=$(echo "${RESULT}" | cut -d: -f5 | cut -d\} -f1)
-        VALUE_FLOAT=$(echo "${VALUE_INTEGER}/${2}" | bc -l | sed -E 's/\.?0+$//' | sed -E 's/^$/0/'1)
-        LINES="${LINES}
+        if [[ ( "${REGISTER_TYPE}" != "uI32" || "${VALUE_INTEGER}" -ne 4294967295 ) && "${VALUE_INTEGER}" -ne 65535 ]]; then
+            if [[ "${REGISTER_TYPE}" == "sI16" && "${VALUE_INTEGER}" -gt 32767 ]]; then
+                VALUE_INTEGER=$(( (65536 - VALUE_INTEGER)*(-1) ))
+            fi
+            VALUE_FLOAT=$(echo "${VALUE_INTEGER}/${2}" | bc -l | sed -E 's/\.?0+$//' | sed -E 's/^$/0/'1)
+            LINES="${LINES}
 ${3},_inverter=${5} value=${VALUE_FLOAT}   $(date +%s%N)"
+        fi
         ERROR_COUNTER="0"
     else
         ((ERROR_COUNTER++))
@@ -68,25 +73,25 @@ while true; do
         echo "ID: ${ID} NAME: ${NAME}" >&2
         if [[ -n ${ID} ]]; then
             if [[ "${PARAMETER_TIME}" == "true" ]]; then
-                readRegisterToLines SiControlUptime 1 time "${ID}" "${NAME}"
+                readRegisterToLines SiControlUptime               1 time        "${ID}" "${NAME}" "uI32"
             fi
             if [[ "${PARAMETER_AC_POWER}" == "true" ]]; then
-                readRegisterToLines InverterAcPower 10 acpower "${ID}" "${NAME}"
+                readRegisterToLines InverterAcPower              10 acpower     "${ID}" "${NAME}" "sI16"
             fi
             if [[ "${PARAMETER_DC_VOLTAGE}" == "true" ]]; then
-                readRegisterToLines InverterDcVoltage 100 dcvoltage "${ID}" "${NAME}"
+                readRegisterToLines InverterDcVoltage           100 dcvoltage   "${ID}" "${NAME}" "uI16"
             fi
             if [[ "${PARAMETER_OPERATING_STATE}" == "true" ]]; then
-                readRegisterToLines InverterVendorOperatingState 1 state "${ID}" "${NAME}"
+                readRegisterToLines InverterVendorOperatingState  1 state       "${ID}" "${NAME}" "uI16"
             fi
             if [[ "${PARAMETER_TEMPERATURE}" == "true" ]]; then
-                readRegisterToLines InverterTemperature 10 temperature "${ID}" "${NAME}"
+                readRegisterToLines InverterTemperature          10 temperature "${ID}" "${NAME}" "sI16"
             fi
             if [[ "${PARAMETER_AC_VOLTAGE}" == "true" ]]; then
-                readRegisterToLines InverterPhaseVoltageA 1 acvoltage "${ID}" "${NAME}"
+                readRegisterToLines InverterPhaseVoltageA         1 acvoltage   "${ID}" "${NAME}" "uI16"
             fi
             if [[ "${PARAMETER_ENERGY_PRODUCTION}" == "true" ]]; then
-                readRegisterToLines InverterWattHours 10 energy "${ID}" "${NAME}"
+                readRegisterToLines InverterWattHours            10 energy      "${ID}" "${NAME}" "uI32"
             fi
         fi
     done
