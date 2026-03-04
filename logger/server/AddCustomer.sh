a67983e5a22be61534f319a454c494baa514fdb1
Maik Urbannek <maik.urbannek@cattatech.de>
2026-03-04 09:29:39 +0100

    logger: Add AddCustomer.

---
+++ logger/server/AddCustomer.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+
+SOURCE="$(readlink -f ${BASH_SOURCE[0]})"
+ROOT="${SOURCE%/*}"
+unset SOURCE
+
+CUSTOMER="${1}"
+shift
+GRAFANA_GROUP="${1}"
+shift
+INFLUXDB_HOST="${1}"
+shift
+INFLUXDB_TOKEN="${1}"
+shift
+INFLUXDB_ORG="${1}"
+shift
+GRAFANA_HOST="${1}"
+shift
+GRAFANA_USER_PASSWORD="${1}"
+shift
+
+TAIL=(     ""    "10s"  "1m"    "test")
+RETENTION=("30d" "356d" "3560d" "12h")
+SIZE=4
+
+INVERTER_COUNT="2"
+
+BUCKET_ID=()
+
+log()
+{
+    echo "${1}" >&2
+}
+
+check()
+{
+    if [[ "${1}" != 0 ]]; then
+        echo "${2}" >&2
+        exit
+    fi
+}
+
+
+create_bucket()
+{
+    log ""
+    log "create_bucket"
+    log "influx bucket create --name \"${1}\"    --retention \"${2}\"   --host \"${INFLUXDB_HOST}\" -t \"${INFLUXDB_TOKEN}\""
+    local TEXT="$(influx bucket create --name "${1}" --retention "${2}"   --host "${INFLUXDB_HOST}" -t "${INFLUXDB_TOKEN}")"
+    local RESULT="${?}"
+    log "${TEXT}"
+    check "${RESULT}" "create bucket"
+    local LAST_LINE="${TEXT##*$'\n'}"
+    local FIRST_ENTRY="${LAST_LINE%%$'\t'*}"
+
+    echo "${FIRST_ENTRY}"
+}
+
+write_token()
+{
+    log ""
+    log "write_token"
+    local CMD=( influx auth create --org "solarinvert" --description "${CUSTOMER}_WRITE")
+    for ((i=0; i<SIZE; i++)); do
+        CMD+=( --write-bucket "${BUCKET_ID[$i]}" )
+    done
+    CMD+=( --host "${INFLUXDB_HOST}" -t "${INFLUXDB_TOKEN}" )
+    log $(echo ${CMD[@]})
+    local TEXT="$("${CMD[@]}")"
+    local RESULT="${?}"
+    log "${TEXT}"
+    check "${RESULT}" "write token"
+    local LAST_LINE="${TEXT##*$'\n'}"
+    local THIRD_ENTRY="$(echo "$LAST_LINE" | cut -f3)"
+    echo "${THIRD_ENTRY}"
+}
+
+read_token()
+{
+    log ""
+    log "read_token"
+    local CMD=( influx auth create --org "solarinvert" --description "${CUSTOMER}_READ")
+    for ((i=0; i<SIZE; i++)); do
+        CMD+=( --read-bucket "${BUCKET_ID[$i]}" )
+    done
+    CMD+=( --host "${INFLUXDB_HOST}" -t "${INFLUXDB_TOKEN}" )
+    log $(echo ${CMD[@]})
+    local TEXT="$("${CMD[@]}")"
+    local RESULT="${?}"
+    log "${TEXT}"
+    check "${RESULT}" "read token"
+    local LAST_LINE="${TEXT##*$'\n'}"
+    local THIRD_ENTRY="$(echo "$LAST_LINE" | cut -f3)"
+    echo "${THIRD_ENTRY}"
+}
+
+datasource()
+{
+    local NAME="${1}"
+    local URL="${2}"
+    local ORG="${3}"
+    local TOKEN="${4}"
+    local USER_PASSWORD="${5}"
+    local HOST="${6}"
+    log ""
+    log "Grafana datasource"
+
+    local JSON="{\"name\":\"${NAME}\",\"type\":\"influxdb\",\"access\":\"proxy\",\"url\":\"${URL}\",\"jsonData\":{\"version\":\"Flux\",\"organization\": \"${ORG}\"},\"secureJsonData\":{\"token\":\"${TOKEN}\"}}"
+    log "${JSON}"
+
+    log "curl -X POST -u '${USER_PASSWORD}' -H 'Content-Type: application/json' -d '${JSON}' '${HOST}/api/datasources'"
+    local TEXT=$(curl -X POST -u "${USER_PASSWORD}" -H "Content-Type: application/json" -d "${JSON}" "${HOST}/api/datasources")
+    local RESULT="${?}"
+    log "${TEXT}"
+    check "${RESULT}" "read token"
+    jq -r '.datasource.uid' <<< "${TEXT}"
+}
+
+GRAFANA_VALUES_WIND_ONLY=( "WIND_SINGLE" )
+GRAFANA_VALUES_WIND=(      "WIND_MULTI" "AC_POWER" "DC_VOLTAGE" "ENERGY_PRODUCTION" "AC_VOLTAGE" "TEMPERATURE" "TIME" "OPERATING_STATE" )
+GRAFANA_VALUES_SOLAR=(                  "AC_POWER" "DC_VOLTAGE" "ENERGY_PRODUCTION" "AC_VOLTAGE" "TEMPERATURE" "TIME" "OPERATING_STATE" )
+
+create_dashboard()
+{
+    local CUSTOMER="${1}"
+    local POSTFIX="${2}"
+    local GRAFANA_DATASOURCE="${3}"
+    local NUMBER_OF_INVERTER="${4}"
+    local -n VALUES="$5"
+    local GRAFANA_USER_PASSWORD="${6}"
+    local GRAFANA_HOST="${7}"
+    log ""
+    log "create_dashboard"
+    dashboard_cmd=("${ROOT}/../server/DashboardContent.sh" "${CUSTOMER}_${POSTFIX}" "${CUSTOMER}" "${GRAFANA_DATASOURCE}" "${NUMBER_OF_INVERTER}" "${VALUES[@]}")
+    log "$(echo "${dashboard_cmd[@]}")"
+    local DASHBOARD="$("${dashboard_cmd[@]}")"
+    local curl_cmd=(curl -X POST -u "$GRAFANA_USER_PASSWORD" -H "Content-Type: application/json" -d "${DASHBOARD}"  "${GRAFANA_HOST}/api/dashboards/db")
+    log "$(echo "${curl_cmd[@]}")"
+    local TEXT="$("${curl_cmd[@]}")"
+    local RESULT="${?}"
+    log "${TEXT}"
+    check "${RESULT}" "create dashboard"
+    jq -r '.id' <<< "${TEXT}"
+}
+
+create_test_dashboard()
+{
+    local CUSTOMER="${1}"
+    local GRAFANA_DATASOURCE="${2}"
+    local -n VALUES="$3"
+    local GRAFANA_USER_PASSWORD="${4}"
+    local GRAFANA_HOST="${5}"
+    log ""
+    log "create_dashboard"
+    dashboard_cmd=("${ROOT}/../server/TestDashboardContent.sh" "${CUSTOMER}_test" "${CUSTOMER}" "${GRAFANA_DATASOURCE}" "${VALUES[@]}")
+    log "$(echo "${dashboard_cmd[@]}")"
+    local DASHBOARD="$("${dashboard_cmd[@]}")"
+    local curl_cmd=(curl -X POST -u "$GRAFANA_USER_PASSWORD" -H "Content-Type: application/json" -d "${DASHBOARD}"  "${GRAFANA_HOST}/api/dashboards/db")
+    log "$(echo "${curl_cmd[@]}")"
+    local TEXT="$("${curl_cmd[@]}")"
+    local RESULT="${?}"
+    log "${TEXT}"
+    check "${RESULT}" "create dashboard"
+    jq -r '.id' <<< "${TEXT}"
+}
+
+create_password()
+{
+    local PASSWORD=$(openssl rand -base64 24)
+    log ""
+    log "create_pass"
+    log "PASSWORD ${PASSWORD}"
+    echo "${PASSWORD}"
+}
+
+add_user()
+{
+    local CUSTOMER="${1}"
+    local PASSWORD="${2}"
+    local GRAFANA_USER_PASSWORD="${3}"
+    local GRAFANA_HOST="${4}"
+
+    local JSON="{\"login\":\"${CUSTOMER}\",\"password\":\"${PASSWORD}\"}"
+    log "${JSON}"
+
+    log "curl -X POST -u '${GRAFANA_USER_PASSWORD}' -H 'Content-Type: application/json' -d '${JSON}' '${GRAFANA_HOST}/api/admin/users'"
+    local TEXT=$(curl -X POST -u "${GRAFANA_USER_PASSWORD}" -H "Content-Type: application/json" -d "${JSON}" "${GRAFANA_HOST}/api/admin/users")
+    local RESULT="${?}"
+    log "${TEXT}"
+    check "${RESULT}" "add user"
+    jq -r '.id' <<< "${TEXT}"
+}
+
+
+set_dashboard_rights()
+{
+    local DASHBOARD_ID="${1}"
+    local TAEM_ID="${2}"
+    local USER_ID="${3}"
+    local GRAFANA_USER_PASSWORD="${4}"
+    local GRAFANA_HOST="${5}"
+
+
+    local JSON="{\"items\":[{\"teamId\":1,\"permission\":1}"
+    if [[ -n "${TAEM_ID}" ]]; then
+        JSON="${JSON},{\"teamId\":${TAEM_ID},\"permission\":1}"
+    fi
+    if [[ -n "${USER_ID}" ]]; then
+        JSON="${JSON},{\"userId\":${USER_ID},\"permission\":1}"
+    fi
+    JSON="${JSON}]}"
+    log "${JSON}"
+
+
+    log "curl -X POST -u '${GRAFANA_USER_PASSWORD}' -H 'Content-Type: application/json' -d '${JSON}' '${GRAFANA_HOST}/api/dashboards/id/${DASHBOARD_ID}/permissions'"
+    local TEXT=$(curl -X POST -u "${GRAFANA_USER_PASSWORD}" -H "Content-Type: application/json" -d "${JSON}" "${GRAFANA_HOST}/api/dashboards/id/${DASHBOARD_ID}/permissions")
+    local RESULT="${?}"
+    log "${TEXT}"
+    check "${RESULT}" "set dashboard rights"
+
+}
+
+create_buckets()
+{
+    for ((i=0; i<SIZE; i++)); do
+        log ""
+        log "create_bucket ${CUSTOMER}${TAIL[$i]} ${RETENTION[$i]}"
+        BUCKET_ID[$i]="$(create_bucket "${CUSTOMER}${TAIL[$i]}" "${RETENTION[$i]}")"
+    done
+}
+
+create_buckets
+
+WRITE_TOKEN="$(write_token)"
+READ_TOKEN="$(read_token)"
+
+DATASOURCE="$(datasource "Source${CUSTOMER}" "http://localhost:8085" "solarinvert" "${READ_TOKEN}" "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}")"
+
+PASSWORD="$( create_password )"
+
+USER="$(add_user "${CUSTOMER}" "${PASSWORD}" "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}" )"
+
+
+
+DASHBOARD_1="$( create_dashboard      "${CUSTOMER}" wind_only  "${DATASOURCE}" "${INVERTER_COUNT}" GRAFANA_VALUES_WIND_ONLY  "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}" )"
+DASHBOARD_2="$( create_dashboard      "${CUSTOMER}" wind       "${DATASOURCE}" "${INVERTER_COUNT}" GRAFANA_VALUES_WIND       "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}" )"
+DASHBOARD_3="$( create_dashboard      "${CUSTOMER}" solar      "${DATASOURCE}" "${INVERTER_COUNT}" GRAFANA_VALUES_SOLAR      "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}" )"
+DASHBOARD_4="$( create_test_dashboard "${CUSTOMER}"            "${DATASOURCE}"                     GRAFANA_VALUES_SOLAR      "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}" )"
+
+set_dashboard_rights "${DASHBOARD_1}" "${GRAFANA_GROUP}" "${USER}" "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}"
+set_dashboard_rights "${DASHBOARD_2}" "${GRAFANA_GROUP}" "${USER}" "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}"
+set_dashboard_rights "${DASHBOARD_3}" "${GRAFANA_GROUP}" "${USER}" "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}"
+set_dashboard_rights "${DASHBOARD_4}" "${GRAFANA_GROUP}" ""        "${GRAFANA_USER_PASSWORD}" "${GRAFANA_HOST}"
+
+echo ""
+echo "./client_install.sh --influxDB ${PASSWORD} --remote https://iot.solarinvert.de:8086 ${BUCKET_ID[@]} ${WRITE_TOKEN} ${INFLUXDB_ORG} --grafana ${INVERTER_COUNT} --inverter 1 /dev/ttyUSB0 --wind 10 5 0.4 --modbuslib --provision ${CUSTOMER} ${CUSTOMER}! wlan0 eth0 60 300 300"
+echo ""
+echo "Name:               ${CUSTOMER}"
+echo "Password:           ${PASSWORD}"
+echo "Grafana user:       ${USER}"
+echo "Grafana group:      ${GROUP}"
+echo "Server Write Token: ${WRITE_TOKEN}"
+echo "Server Read Token:  ${READ_TOKEN}"
+echo "Datasource:         ${DATASOURCE}"
---
+++ logger/server/TestDashboardContent.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+diagram()
+{
+    local X="0"
+    local W="24"
+
+    local ID="${1}"
+    local Y="${2}"
+    local NAME="${3}"
+    local MIN="${4}"
+    local MAX="${5}"
+    local RED="${6}"
+    local UNIT="${7}"
+    local INFLUXDB_BUCKET_TEST="${8}"
+    local INFLUXDB_NAME="${9}"
+    local INFLUXDB_DATASOURCE="${10}"
+    local LINE="${11}"
+    local FUNCTION="${12}"
+    local CONTENT="{\"datasource\":{\"type\":\"influxdb\",\"uid\":\"${INFLUXDB_DATASOURCE}\"},\"fieldConfig\":{\"defaults\":{\"color\":{\"mode\":\"palette-classic\"},\"custom\":{\"axisBorderShow\":false,\"axisCenteredZero\":false,\"axisColorMode\":\"text\",\"axisLabel\":\"\",\"axisPlacement\":\"auto\",\"barAlignment\":0,\"barWidthFactor\":0.6,\"drawStyle\":\"line\",\"fillOpacity\":0,\"gradientMode\":\"none\",\"hideFrom\":{\"legend\":false,\"tooltip\":false,\"viz\":false},\"insertNulls\":3600000,\"lineInterpolation\":\"${LINE}\",\"lineWidth\":1,\"pointSize\":5,\"scaleDistribution\":{\"type\":\"linear\"},\"showPoints\":\"never\",\"spanNulls\":false,\"stacking\":{\"group\":\"A\",\"mode\":\"none\"},\"thresholdsStyle\":{\"mode\":\"off\"}},\"mappings\":[],"
+    if [ -n "${MAX}" ]; then
+        CONTENT="${CONTENT}\"max\":${MAX},"
+    fi
+    if [ -n "${MIN}" ]; then
+        CONTENT="${CONTENT}\"min\":${MIN},"
+    fi
+    CONTENT="${CONTENT}\"thresholds\":{\"mode\":\"absolute\",\"steps\":[{\"color\":\"green\",\"value\":"
+    if [ -n "${RED}" ]; then
+        CONTENT="${CONTENT}${MIN}},{\"color\":\"red\",\"value\":${RED}"
+    else
+        CONTENT="${CONTENT}null"
+    fi
+    CONTENT="${CONTENT}}]}"
+    if [ -n "${UNIT}" ]; then
+        CONTENT="${CONTENT},\"unit\":\"${UNIT}\""
+    fi
+    CONTENT="${CONTENT}},\"overrides\":[{\"matcher\":{\"id\":\"byName\",\"options\":\"raw\"},\"properties\":[{\"id\":\"custom.insertNulls\",\"value\":10000}]},{\"matcher\":{\"id\":\"byName\",\"options\":\"10s\"},\"properties\":[{\"id\":\"custom.insertNulls\",\"value\":10000}]},{\"matcher\":{\"id\":\"byName\",\"options\":\"1m\"},\"properties\":[{\"id\":\"custom.insertNulls\",\"value\":60000}]},{\"matcher\":{\"id\":\"byName\",\"options\":\"1m\"},\"properties\":[{\"id\":\"custom.insertNulls\",\"value\":60000}]},{\"matcher\":{\"id\":\"byName\",\"options\":\"10m\"},\"properties\":[{\"id\":\"custom.insertNulls\",\"value\":600000}]},{\"matcher\":{\"id\":\"byName\",\"options\":\"1h\"},\"properties\":[{\"id\":\"custom.insertNulls\",\"value\":3600000}]},{\"matcher\":{\"id\":\"byName\",\"options\":\"6h\"},\"properties\":[{\"id\":\"custom.insertNulls\",\"value\":21600000}]},{\"matcher\":{\"id\":\"byName\",\"options\":\"1d\"},\"properties\":[{\"id\":\"custom.insertNulls\",\"value\":86400000}]}]},\"gridPos\":{\"h\":6,\"w\":${W},\"x\":${X},\"y\":${Y}},\"id\":${ID},\"options\":{\"legend\":{\"calcs\":[],\"displayMode\":\"list\",\"placement\":\"bottom\",\"showLegend\":true},\"tooltip\":{\"hideZeros\":false,\"mode\":\"single\",\"sort\":\"none\"}},\"pluginVersion\":\"12.1.0\",\"targets\":[{\"datasource\":{\"type\":\"influxdb\",\"uid\":\"${INFLUXDB_DATASOURCE}\"},\"hide\":false,\"query\":\"import \\\"experimental\\\"\\n\\nstart = v.timeRangeStart\\nstop = v.timeRangeStop\\n\\nbucket_name = \\\"${INFLUXDB_BUCKET_TEST}\\\"\\n\\nwindow_ns = int(v: v.windowPeriod)\\nwindow = if window_ns <= int(v: 10s) then 10s else v.windowPeriod\\n\\nfield_label = if window_ns <= int(v: 10s) then\\n    \\\"10s\\\"\\nelse if window_ns <= int(v: 1m) then\\n    \\\"1m\\\"\\nelse if window_ns <= int(v: 10m) then\\n    \\\"10m\\\"\\nelse if window_ns <= int(v: 1h) then\\n    \\\"1h\\\"\\nelse if window_ns <= int(v: 6h) then\\n    \\\"6h\\\"\\nelse\\n    \\\"1d\\\"\\n\\ndata = from(bucket: bucket_name)\\n|> range(start: start, stop: stop)\\n|> filter(fn: (r) => r._measurement == \\\"${INFLUXDB_NAME}\\\")\\n|> aggregateWindow(every:window, fn: ${FUNCTION})\\n|> set(key: \\\"_field\\\", value: field_label)\\n|> yield(name: \\\"auto\\\")\",\"refId\": \"A\"}],\"title\":\"${NAME}\",\"type\":\"timeseries\"}"
+    cat <<EOF
+${CONTENT}
+EOF
+}
+
+dashboard_head()
+{
+    local CONTENT="{\"dashboard\":{\"annotations\":{\"list\":[{\"builtIn\":1,\"datasource\":{\"type\":\"grafana\",\"uid\":\"-- Grafana --\"},\"enable\":true,\"hide\":true,\"iconColor\":\"rgba(0, 211, 255, 1)\",\"name\":\"Annotations & Alerts\",\"type\":\"dashboard\"}]},\"editable\":true,\"fiscalYearStartMonth\":0,\"graphTooltip\":0,\"id\":null,\"links\":[],\"panels\":["
+    cat <<EOF
+${CONTENT}
+EOF
+}
+
+dashboard_tail()
+{
+    local NAME="${1}"
+    local CONTENT="],\"preload\":false,\"schemaVersion\":41,\"tags\":[],\"templating\":{\"list\":[]},\"time\":{\"from\":\"now-30m\",\"to\":\"now\"},\"timepicker\":{},\"timezone\":\"browser\",\"title\":\"${NAME}\",\"permissions\":[{\"role\":\"Admin\",\"permission\":4}]},\"folderId\": 0,\"overwrite\": true}"
+    cat <<EOF
+${CONTENT}
+EOF
+}
+
+handle_Type()
+{
+    local RESULT=""
+    local NUMBER="${2}"
+    local BUCKET="${3}"
+    local INFLUXDB_DATASOURCE="${4}"
+    local Y="$((NUMBER * 6))"
+    case ${1} in
+      TIME)
+        RESULT="${RESULT}$(diagram "${NUMBER}" "${Y}" "Up Time" "" "" "" "dtdhms" "${BUCKET}test" "time" "${INFLUXDB_DATASOURCE}" "smooth" "mean")"
+        ;;
+      AC_POWER)
+        RESULT="${RESULT}$(diagram "${NUMBER}" "${Y}" "AC Power" "" "" "" "watt" "${BUCKET}test" "acpower" "${INFLUXDB_DATASOURCE}" "smooth" "mean")"
+        ;;
+      DC_VOLTAGE)
+        RESULT="${RESULT}$(diagram "${NUMBER}" "${Y}" "DC Voltage" "" "" "" "volt" "${BUCKET}test" "dcvoltage" "${INFLUXDB_DATASOURCE}" "smooth" "mean")"
+        ;;
+      OPERATING_STATE)
+        RESULT="${RESULT}$(diagram "${NUMBER}" "${Y}" "State" 0 11 "" "" "${BUCKET}test" "state" "${INFLUXDB_DATASOURCE}" "stepBefore" "first")"
+        ;;
+      TEMPERATURE)
+        RESULT="${RESULT}$(diagram "${NUMBER}" "${Y}" "Temperature" "" "" "" "celsius" "${BUCKET}test" "temperature" "${INFLUXDB_DATASOURCE}" "smooth" "mean")"
+        ;;
+      AC_VOLTAGE)
+        RESULT="${RESULT}$(diagram "${NUMBER}" "${Y}" "AC Voltage" "" "" "" "volt" "${BUCKET}test" "acvoltage" "${INFLUXDB_DATASOURCE}" "smooth" "mean")"
+        ;;
+      ENERGY_PRODUCTION)
+        RESULT="${RESULT}$(diagram "${NUMBER}" "${Y}" "Energy Production" "" "" "" "watth" "${BUCKET}test" "energy" "${INFLUXDB_DATASOURCE}" "smooth" "mean")"
+        ;;
+      WIND)
+        RESULT="${RESULT}$(diagram "${NUMBER}" "${Y}" "Wind" "" "" "" "velocityms" "${BUCKET}test" "wind" "${INFLUXDB_DATASOURCE}" "smooth" "mean")"
+        ;;
+      *)
+        echo "handle_Type: '${1}' is not a valid type. [TIME,AC_POWER,DC_VOLTAGE,OPERATING_STATE,TEMPERATURE,AC_VOLTAGE,AC_VOLTAGE,ENERGY_PRODUCTION]" 1  >&2
+        exit 1
+      ;;
+    esac
+    cat <<EOF
+${RESULT}
+EOF
+}
+
+
+CONTENT="$(dashboard_head)"
+I="1"
+NAME="${1}"
+shift
+BUCKET="${1}"
+shift
+INFLUXDB_DATASOURCE="${1}"
+shift
+while [[ ${#} -gt 0 ]]; do
+    CONTENT="${CONTENT}$(handle_Type "${1}" "${I}" "${BUCKET}" "${INFLUXDB_DATASOURCE}")"
+    shift
+    if [[ ${#} -gt 0 ]]; then
+        CONTENT="${CONTENT},"
+    fi
+    I="$((I + 1))"
+done
+CONTENT="${CONTENT}$(dashboard_tail "${NAME}")"
+cat <<EOF
+${CONTENT}
+EOF
